2358117922e223997706c9aa955b614f084f781b
marina33311 <https://github.com/marina33311/modul16>
2022-11-04 15:19:51 +0300

    start_fiels

--- Marina_2021/blog/models.py
+++ Marina_2021/blog/models.py
@@ -11,12 +11,12 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
-# Create your models here.
+
 class Comment(models.Model):
-    post = models.ForeignKey('Comment', on_delete=models.CASCADE)
     author = models.CharField(max_length=80)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
 
 
     def __str__(self):
